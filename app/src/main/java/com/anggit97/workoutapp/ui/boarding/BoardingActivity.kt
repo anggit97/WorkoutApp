fa1eeb8d524531ba3481079733326c3e4ee44fb8
anggitprayogo <anggitprayogo@gmail.com>
2020-04-28 18:53:58 +0700

    Adding boarding empty activity

---
+++ app/src/main/java/com/anggit97/workoutapp/ui/boarding/BoardingActivity.kt
@@ -0,0 +1,13 @@
+package com.anggit97.workoutapp.ui.boarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anggit97.workoutapp.R
+
+class BoardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_boarding)
+    }
+}
